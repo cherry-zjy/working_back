a47fa88f4891ca68825176cdbbbc59a934770818
zjy <296579033@qq.com>
2018-05-25 15:08:42 +0800

    发放津贴完成

--- src/components/work/enter.vue
+++ src/components/work/enter.vue
@@ -34,19 +34,23 @@
     <el-table :data="List" style="width: 100%" :border='true'>
       <el-table-column label="用户名" prop="Name">
       </el-table-column>
-      <el-table-column label="联系电话" prop="Phone">
+      <el-table-column label="联系电话" prop="Phone" width="150">
       </el-table-column>
-      <el-table-column label="报名时间" prop="Time" :formatter="timefilterHandler">
+      <el-table-column label="报名时间" prop="Time" :formatter="timefilterHandler" width="150">
       </el-table-column>
-      <el-table-column label="经纪人号码" prop="AgentPhone">
+      <el-table-column label="经纪人号码" prop="AgentPhone" width="150">
       </el-table-column>
       <el-table-column label="申请企业" prop="Enterprise">
       </el-table-column>
-      <el-table-column label="入职状态" prop="Type">
+      <el-table-column label="入职状态" prop="Type" width="150">
       </el-table-column>
+      <el-table-column label="入职时间" prop="WorkTime" width="150" :formatter="WorkTime">
+      </el-table-column>
+      
       <el-table-column label="操作">
         <template slot-scope="scope">
           <el-button v-if="scope.row.Type == '未入职'" size="mini" type="primary" @click="entry(scope.row.ID)">入职</el-button>
+          <el-button v-if="scope.row.Type == '已入职'" size="mini" type="warning" @click="send(scope.row.ID)">发放津贴</el-button>
           <el-button size="mini" type="danger" plain icon="el-icon-delete" @click="handleDel(scope.row.ID)">删除</el-button>
         </template>
       </el-table-column>
@@ -57,6 +61,19 @@
       <el-pagination @current-change="handleCurrentChange" layout="prev, pager, next,jumper" :page-count="pageCount">
       </el-pagination>
     </div>
+
+    <!-- 模态框 -->
+    <el-dialog title="发放津贴" :visible.sync="dialogFormVisible" width="30%">
+      <el-form :model="money" :rules="rules" ref="money" label-width="100px" class="demo-ruleForm">
+        <el-form-item label="金额" prop="Money">
+          <el-input v-model="money.Money"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="submitForm('money')">确 定</el-button>
+      </div>
+    </el-dialog>
 
   </div>
 </template>
@@ -74,6 +91,15 @@
           StartTime: "",
           EndTime: "",
           Type:-1
+        },
+        money:[],
+        dialogFormVisible:false,
+        rules: {
+            Money: [{
+            required: true,
+            message: '请输入金额',
+            trigger: 'blur'
+          }, ],
         },
         typeList:[{
             name: "全部",
@@ -158,6 +184,15 @@
         Time = Time.substring(0, 10);
         return Time
       },
+      WorkTime(row) {
+        var WorkTime = row.WorkTime;
+        if (WorkTime !== null) {
+          WorkTime = WorkTime.substring(0, 10);
+        }
+        return WorkTime
+      },
+      
+      //重置按钮
       getAllUsers() {
         this.filters = {
           keyword: "",
@@ -165,6 +200,10 @@
           EndTime: "",
           Type: -1
         }
+      },
+      send(id){
+        this.money.ID = id;
+        this.dialogFormVisible = true;
       },
       //删除
       handleDel(id){
@@ -231,7 +270,7 @@
         }).catch(() => {
           this.$message({
             type: 'info',
-            message: '已取消删除'
+            message: '已取消'
           });
         });
       },
@@ -240,7 +279,6 @@
         this.pageIndex = val;
         this.getInfo();
       },
-
       entry(id) {
         this.$confirm('确认入职?', '提示', {
           confirmButtonText: '确定',
@@ -307,6 +345,75 @@
             type: 'info',
             message: '已取消删除'
           });
+        });
+      },
+      //发放津贴
+      submitForm(formName) {
+        this.$refs[formName].validate((valid) => {
+          if (valid) {
+            const loading = this.$loading({
+                lock: true,
+                text: "Loading",
+                spinner: "el-icon-loading",
+                background: "rgba(0, 0, 0, 0.7)"
+              });
+            this.$http
+            .get("api/Back/SendMoney", {
+              params: {
+                Token: getCookie("token"),
+                ID: this.money.ID,
+                Money: this.money.Money,
+              }
+            })
+            .then(
+              function (response) {
+                loading.close();
+                var status = response.data.Status;
+                if (status === 1) {
+                  this.$message({
+                    showClose: true,
+                    type: "success",
+                    message: response.data.Result
+                  });
+                  this.dialogFormVisible = false;
+                  this.getInfo();
+                } else if (status === 40001) {
+                  this.$message({
+                    showClose: true,
+                    type: "warning",
+                    message: response.data.Result
+                  });
+                  setTimeout(() => {
+                    this.$router.push({
+                      path: "/login"
+                    });
+                  }, 1500);
+                } else {
+                  loading.close();
+                  this.$message({
+                    showClose: true,
+                    type: "warning",
+                    message: response.data.Result
+                  });
+                }
+              }.bind(this)
+            )
+            // 请求error
+            .catch(
+              function (error) {
+                loading.close();
+                this.$notify.error({
+                  title: "错误",
+                  message: "错误：请检查网络"
+                });
+              }.bind(this)
+            );
+            this.dialogFormVisible = false;
+            this.editlist = [];
+          } else {
+            console.log('error submit!!');
+            return false;
+          }
         });
       },
     },
